d4d7db867f7fcd0698e7288321fb7fea2bf9c340
Arseny <me@evindor.com>
2017-03-29 14:13:37 +0300

    Initial Commit

---
+++ Spacelight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spacelight
+//
+//  Created by Arseny Zarechnev on 29/03/2017.
+//  Copyright © 2017 Arseny Zarechnev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
